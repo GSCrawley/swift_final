f11e97d5ed52306ab3a8300132732d4ae7b67bc3
GSCrawley <gscrawley@gmail.com>
2021-03-02 17:33:32 -0800

    oops forgot to commit Succeeded in building two circles with one slider for each color that change the color of the circles according to the rgb values

--- color_match/color_match/ContentView.swift
+++ color_match/color_match/ContentView.swift
@@ -9,13 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    static func randomiser() -> Double {
+        return Double.random(in: 0..<1)
+    }
+    
+    @State var redActual = randomiser()
+    @State var greenActual = randomiser()
+    @State var blueActual = randomiser()
+    
+    @State var redSlider: Double
+    @State var greenSlider: Double
+    @State var blueSlider: Double
+
+
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20){
+            Text("Match this Color")
+                .font(.largeTitle)
+                .bold()
+            HStack{
+                Circle()
+                    .fill(Color(red: redActual, green: greenActual,
+                                blue: blueActual))
+                    .padding()
+                Circle().fill(Color(red: redSlider, green: greenSlider, blue: blueSlider))
+                    .padding()
+            }
+            
+            Sliders(value: $redSlider, color: .red, textColor: "Red")
+            Sliders(value: $greenSlider, color: .green, textColor: "Green")
+            Sliders(value: $blueSlider, color: .blue, textColor: "Blue")
+            
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(redSlider: 0.0, greenSlider: 0.0, blueSlider: 0.0)
     }
 }
+
+struct Sliders: View {
+    @Binding var value: Double
+    var color: Color
+    var textColor: String
+    
+    var body: some View {
+        VStack{
+            Text("\(textColor) (\(Int(value * 255) )) ")
+                .font(.largeTitle)
+            Slider(value: $value)
+                .accentColor(.red)
+                .padding()
+        }
+    }
+}
+ 
